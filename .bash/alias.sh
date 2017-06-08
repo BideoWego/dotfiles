b410f531bc040500298bd9479140776002992f3c
Bideo Wego <bideowego@gmail.com>
2017-06-08 13:30:18 -0400

    Add trello alias

--- .bash/alias.sh
+++ .bash/alias.sh
@@ -54,6 +54,7 @@ alias pg='postgres -D /usr/local/var/postgres'
 alias github='open https://github.com'
 alias bitbucket='open https://bitbucket.org'
 alias asana='open https://app.asana.com'
+alias trello='open https://trello.com'
 alias pivotaltracker='open https://www.pivotaltracker.com/dashboard'
 alias flushdns='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder; echo "DNS Flushed";'
 alias rmds_store="find . -name '*.DS_Store' -type f -delete"
