fba65fe7ee6537bc31c63c5764c1616c321b02e2
Bideo Wego <bideowego@gmail.com>
2017-08-01 13:03:28 -0400

    Update aliases

--- .bash/alias.sh
+++ .bash/alias.sh
@@ -13,6 +13,7 @@ alias apps='cd ~/Applications'
 alias desktop='cd ~/Desktop'
 alias documents='cd ~/Documents'
 
+
 #--------------------------------------------------------
 # Apps
 #--------------------------------------------------------
@@ -24,6 +25,7 @@ alias opera='open -a opera'
 alias preview='open -a preview'
 alias xcode='open -a xcode'
 
+
 #--------------------------------------------------------
 # Command Shorthands
 #--------------------------------------------------------
@@ -31,6 +33,7 @@ alias xcode='open -a xcode'
 alias ll='ls -lahGF'
 alias c='clear'
 alias h='history'
+
 
 #========================================================
 # Rewrite existing commands to make the more secure
@@ -46,21 +49,66 @@ alias df='df -h'
 alias du='df -h'
 alias mkdir='mkdir -v -p'
 
+
 #========================================================
-# Misc Aliases
+# Databases
 #========================================================
 
 alias pg='postgres -D /usr/local/var/postgres'
+alias mg="mongod --port 27017 --dbpath /usr/local/var/mongodb"
+alias redis="redis-server /usr/local/etc/redis.conf"
+
+
+# ----------------------------------------
+# Version Control
+# ----------------------------------------
+
 alias github='open https://github.com'
 alias bitbucket='open https://bitbucket.org'
+
+
+# ----------------------------------------
+# Productivity
+# ----------------------------------------
+
 alias asana='open https://app.asana.com'
 alias trello='open https://trello.com'
 alias pivotaltracker='open https://www.pivotaltracker.com/dashboard'
+alias googlesheets='open https://docs.google.com/spreadsheets'
+alias googledocs='open https://docs.google.com/document'
+alias googlemail='open https://mail.google.com/mail'
+alias gmail='open https://mail.google.com/mail'
+alias vcs='open https://www.vikingcodeschool.com'
+
+
+# ----------------------------------------
+# Mac
+# ----------------------------------------
+
 alias flushdns='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder; echo "DNS Flushed";'
 alias rmds_store="find . -name '*.DS_Store' -type f -delete"
+
+
+# ----------------------------------------
+# SASS
+# ----------------------------------------
+
 alias sassw='sass --watch assets/stylesheets/scss:assets/stylesheets/css --no-cache'
-alias mg="mongod --port 27017 --dbpath /usr/local/var/mongodb"
-alias redis="redis-server /usr/local/etc/redis.conf"
+
+
+# ----------------------------------------
+# Text Editor
+# ----------------------------------------
+
 alias subl='subl -n'
+
+
+
+
+
+
+
+
+
 
 
